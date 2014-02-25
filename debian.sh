d85d346a094c8a5e809a87925a01e7f4ea6176ed
Petr Bena <benapetr@gmail.com>
2014-02-25 16:58:23 +0100

    extra checks

--- debian.sh
+++ debian.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+if [ ! -f huggle/version.txt ];then
+    echo "There is no version.txt file, please generate it first"
+    exit 1
+fi
+
+version=`cat huggle/version.txt`
+
+echo "Generating package for huggle $version"
+
 if [ "$#" -lt 1 ];then
     echo "Enter version"
     read v

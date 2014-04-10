1565ff20439dde1f75a1377d369c542d4a75899a
Petr Bena <benapetr@gmail.com>
2014-04-10 20:59:03 +0200

    saucy needs python .3 and trusty .4

--- ubuntu.sh
+++ ubuntu.sh
@@ -26,6 +26,9 @@ do
     mkdir targets/$target || exit 1
     cp -r "huggle-$v" "targets/$target/huggle-$v" || exit 1
     cp "huggle_$v.orig.tar.gz" "targets/$target/huggle_$v-$target.orig.tar.gz" || exit 1
+    if [ -f "targets.info/"$target"_huggle_pro" ];then
+        cp "targets.info/"$target"_huggle_pro" "targets/$target/huggle-$v/huggle.pro" || exit 1
+    fi
     if [ -f "targets.info/"$target"_control" ];then
         cp "targets.info/"$target"_control" "targets/$target/huggle-$v/debian/control" || exit 1
     fi
